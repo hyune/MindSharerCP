8ce526197e2d032963cfa3775c8ba0149e28a2d3
Hyune <Hyune@nate.com>
2014-11-14 19:34:56 +0900

    gg

---
+++ MindSharerCPM/target/web-1.0.0-BUILD-SNAPSHOT/WEB-INF/js/memberregister.js
@@ -0,0 +1,44 @@
+/**
+ * Created by karu on 2014-10-22.
+ */
+var myModlue = angular.module('MemeberRegisterApp' , []);
+
+myModlue.controller('InputRegisterController', function ($scope, $http)
+{
+    $scope.person1 = {};
+
+    $scope.redundancy = function(uid, isRedundancy){
+    	$http({
+        	method : "POST",
+        	url : "adduser/redundancy",
+        	data : { "uid" : uid }
+        }).success(function (data, status, headers, config)
+            {
+        		if(data.status == 0 && data.result == "Y"){
+        			confirm("사용 가능한 아이디 입니다.");
+        		}        		
+        		$scope[isRedundancy] = true;
+            })
+            .error(function (data, status, headers, config)
+            {
+            	$scope[isRedundancy] = false;
+            });
+    }
+
+    $scope.submitData = function (user, resultVarName)
+    {
+        $http({
+        	method : "POST",
+        	url : "adduser/result",
+        	headers : "application/json",
+        	data : user
+        }).success(function (data, status, headers, config)
+            {
+                $scope[resultVarName] = data;
+            })
+            .error(function (data, status, headers, config)
+            {
+                $scope[resultVarName] = "SUBMIT ERROR";
+            });
+    };
+});
